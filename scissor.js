404c927e0c956fc078abc4c1477658bd0dab7ec0
lilibethlangat <beth13lilib@gmail.com>
2025-04-16 10:21:21 +0100

    create a function that return computer game choice

--- scissor.js
+++ scissor.js
@@ -1,1 +1,30 @@
-console.log("HELLO LILIBETH YOU GOT THIS IN THE BAG HUN")
+function getComputerChoice(){
+    const Number = Math.random();
+    if (Number === 0){
+        return "Rock it";
+    }else if(Number === 1){
+        return "It's a paper";
+    }else{
+        return "scissors";
+    }
+
+
+}
+
+console.log(getComputerChoice());
+
+// function getHumanChoice(humanChoice){
+//    let Choice = prompt("What number do you choose?");
+//    if (Choice <= 1){
+//     return "Rock it";
+//    }else if (Choice <=100 ){
+//     return "It's a paper";
+//    }else if (Choice % 5 === 0){
+//     return "Scissors";
+//    }
+//    else{
+//     return("PLEASE INPUT A NUMBER");
+//    }
+// }
+// console.log(getHumanChoice())
+
